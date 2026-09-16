daa7dcdfe3eecb5d71e6c22638fddd6714245db0
Masoud <masoudmalik95@gmail.com>
2026-09-16 12:49:42 +0430

    Update dark mode and effecs

--- backend/src/db/connectDB.js
+++ backend/src/db/connectDB.js
@@ -7,7 +7,7 @@ const connectDB = async () => {
       process.env.DATABASE_CONNECTION_URL ||
       "mongodb://127.0.0.1:27017";
 
-    await mongoose.connect(`${mongoUrl}/superio-job-portal`);
+    await mongoose.connect(`${mongoUrl}/AFG Job`);
 
     console.log("✅ Database connected successfully");
   } catch (error) {
